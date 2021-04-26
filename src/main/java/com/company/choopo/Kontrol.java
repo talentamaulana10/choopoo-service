6ec87eeff63d6f5b170b4cea677fcd84477bf2cd
Prasetya234 <putraprasetyadhany@gmail.com>
2021-04-26 16:27:31 +0700

    spring boot

---
+++ src/main/java/com/company/choopo/Kontrol.java
@@ -0,0 +1,29 @@
+package com.company.choopo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class Kontrol {
+
+    @GetMapping("/HelloWorld")
+    public  String HelloWorld(){
+        return "Hello World";
+    }
+
+    @GetMapping("/HelloWorld/{name}")
+    public String HelloWorldName(@PathVariable(value="name") String name){
+        return "Hellow " + name;
+    }
+
+    @GetMapping("/customHeader")
+    ResponseEntity<String> customHeader() {
+        return ResponseEntity.ok()
+                .body("Custom header Package");
+    }
+
+}
